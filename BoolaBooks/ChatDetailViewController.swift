4cdd40ba8709ac1f25a068275f509de51413faf5
Apurv Suman <sumanicus@gmail.com>
2017-01-07 16:38:31 -0800

    closes #65, can send messages, can also close channel by going back to all_chats

--- BoolaBooks/ChatDetailViewController.swift
+++ BoolaBooks/ChatDetailViewController.swift
@@ -14,18 +14,51 @@ class ChatDetailViewController: UIViewController {
     
     // MARK: - Properties
     var conversationID: Int?
+    var ws: WebSocket?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        chatListen()
-
-        // Do any additional setup after loading the view.
+        // Initialize WebSocket
+        let prefs = UserDefaults.standard
+        let urlWithParams = "wss://boolabooks.herokuapp.com/cable?token=\(prefs.string(forKey: "rails_token")!)&uid=\(prefs.string(forKey: "fb_uid")!)"
+        let request = NSMutableURLRequest(url: NSURL(string: urlWithParams)! as URL)
+        ws = WebSocket(request: request as URLRequest)
+        
+        // Connect to Channel
+        ws?.event.open = {
+            print("opened")
+            
+            let identifierDict = ["channel": "ConversationsChannel", "conversation_id": self.conversationID!] as [String: Any?]
+            let identifierJSON = JSON(identifierDict)
+            let identifierRawString = identifierJSON.rawString()
+            let dict = ["command": "subscribe", "identifier": identifierRawString ] as [String: Any?]
+            let json = JSON(dict)
+            let msg = json.rawString()
+            
+            self.ws?.send(msg!)
+        }
+        
+        // parse the messasge
+        ws?.event.message = { message in
+            if let text = message as? String {
+                print("received: \(text)")
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    // close channel connection when leaving chat
+    override func viewWillDisappear(_ animated : Bool) {
+        super.viewWillDisappear(animated)
+        
+        if (self.isMovingFromParentViewController){
+            ws?.close()
+        }
     }
     
 
@@ -39,49 +72,25 @@ class ChatDetailViewController: UIViewController {
     }
     */
     
-    // MARK: - BoolaBooks API Calls
-    
-    func chatListen() {
+    // MARK: - Actions
 
-        // Initialize WebSocket
-        let prefs = UserDefaults.standard
-        let urlWithParams = "wss://boolabooks.herokuapp.com/cable?token=\(prefs.string(forKey: "rails_token")!)&uid=\(prefs.string(forKey: "fb_uid")!)"
-        let request = NSMutableURLRequest(url: NSURL(string: urlWithParams)! as URL)
-        let ws = WebSocket(request: request as URLRequest)
+    @IBAction func sendMessage(_ sender: UIButton) {
         
-        // helper function
-        let send : ()->() = {
-            
-            let identifierDict = ["channel": "ConversationsChannel", "conversation_id": self.conversationID!] as [String: Any?]
-            let identifierJSON = JSON(identifierDict)
-            let identifierRawString = identifierJSON.rawString()
-            let dict = ["command": "subscribe", "identifier": identifierRawString ] as [String: Any?]
-            let json = JSON(dict)
-            let msg = json.rawString()
-            
-            ws.send(msg!)
-        }
+        // hard-coded message right now
+        let dataDict = ["conversation_id": self.conversationID!, "text": "iOS CLIENT", "action": "send_message"] as [String: Any?]
+        let dataJSON = JSON(dataDict)
+        let dataRawString = dataJSON.rawString()
         
-        // Connect to Channel
-        ws.event.open = {
-            print("opened")
-            send()
-        }
-//
-//        ws.event.close = { code, reason, clean in
-//            print("close")
-//        }
-//        
-//        ws.event.error = { error in
-//            print("error \(error)")
-//        }
+        let identifierDict = ["channel": "ConversationsChannel", "conversation_id": self.conversationID!] as [String: Any?]
+        let identifierJSON = JSON(identifierDict)
+        let identifierRawString = identifierJSON.rawString()
         
-        // parse the messasge
-        ws.event.message = { message in
-            if let text = message as? String {
-                print("received: \(text)")
-            }
-        }
+        let dict = ["command": "message", "identifier": identifierRawString, "data": dataRawString ] as [String: Any?]
+        let json = JSON(dict)
+        let msg = json.rawString()
+        
+        self.ws?.send(msg!)
+        print("Sent: \(msg!)")
+        
     }
-
 }
